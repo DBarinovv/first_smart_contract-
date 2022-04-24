fa37fad5a5cb321162428ef0f106cc7fdb2fb9f1
DBarinovv <9653314755@bk.ru>
2022-04-24 03:32:53 -0700

    Working example

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,15 @@ use gstd::{msg, String};
 
 #[no_mangle]
 pub unsafe extern "C" fn handle() {
-    let new_msg: String = msg::load().expect("Can't load(");
+    let new_msg = String::from_utf8(msg::load_bytes()).expect("Invalid message");
 
     if new_msg == "Hello" {
-        msg::reply(b"Hello!", 0).unwrap();
+        msg::reply(b"Hello!", 0).expect("Can't send(");
     }
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn init() {}
-
 
 #[cfg(test)]
 mod tests {
@@ -21,15 +20,17 @@ mod tests {
 
     #[test]
     fn test_hello() {
+        const MY_ADDRESS: u64 = 2;
+
         let system = System::new();
         system.init_logger();
         let program = Program::current(&system);
 
-        let res = program.send_bytes(2, "INIT");
+        let res = program.send_bytes(MY_ADDRESS, "INIT");
         assert!(res.log().is_empty());
 
-        let res = program.send_bytes(2, "Hello");
+        let res = program.send_bytes(MY_ADDRESS, "Hello");
         let reply = "Hello!".as_bytes();
-        assert!(res.contains(&(10, reply)));       
+        assert!(res.contains(&(MY_ADDRESS, reply)));
     }
 }
