494d3c951716a4dcb003a9cf8bc14386418e570d
DBarinovv <9653314755@bk.ru>
2022-04-24 05:13:32 -0700

    First try

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,75 @@
 #![no_std]
 
-use gstd::{msg, String};
+use gstd::{msg, lock::mutex::Mutex};
 
 #[no_mangle]
 pub unsafe extern "C" fn handle() {
+
     let new_msg = String::from_utf8(msg::load_bytes()).expect("Invalid message");
 
-    if new_msg == "Hello" {
-        msg::reply(b"Hello!", 0).expect("Can't send(");
+    if new_msg == "Send" {
+
+    }
+    else if new_msg == "Withdraw" {
+
     }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn init() {}
+pub unsafe extern "C" fn init() {
+}
+
+struct Wallet {
+    balance: u64,
+}
+
+impl Wallet {
+    fn new() -> Wallet {
+        Wallet {
+            balance: 0,
+        }
+    }
+
+    fn get_money(&mut self, value: u64) {
+        let quard = Mutex::new(self.balance);
+        quard.lock();
+        self.balance += value;
+    }
+
+    fn send_money(&mut self, value: u64) {
+        let quard = Mutex::new(self.balance);
+        quard.lock();
+        if self.balance < value {
+            msg::reply(b"Error", 0).unwrap();
+        }
+        else {
+            self.balance -= value;
+            msg::reply(b"Ok", 0).unwrap();
+        }
+    }
+
+    fn get_balance(&self) -> u64 {
+        self.balance
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use gtest::{Program, System};
 
     #[test]
-    fn test_hello() {
-        const MY_ADDRESS: u64 = 2;
+    fn test_send() {
+        const MY_ADDRESS: u64 = 5;
 
         let system = System::new();
         system.init_logger();
         let program = Program::current(&system);
 
-        let res = program.send_bytes(MY_ADDRESS, "INIT");
-        assert!(res.log().is_empty());
 
-        let res = program.send_bytes(MY_ADDRESS, "Hello");
-        let reply = "Hello!".as_bytes();
-        assert!(res.contains(&(MY_ADDRESS, reply)));
+    }
+
+    #[test]
+    fn test_withdraw() {
+
     }
 }
